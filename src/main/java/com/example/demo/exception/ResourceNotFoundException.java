82632ece95517ce11ed179230fee53db356d0008
bala-amypo <bala@amypo.in>
2025-12-18 04:20:05 +0000

    User commit

--- src/main/java/com/example/demo/exception/ResourceNotFoundException.java
+++ src/main/java/com/example/demo/exception/ResourceNotFoundException.java
@@ -1,2 +1,4 @@
 package com.example.demo.entity;
-public class ResourceNotFoundException
+public class ResourceNotFoundException{
+    
+}
---
+++ src/main/java/com/example/demo/serviceImplementation/Studentservice.java
@@ -0,0 +1,5 @@
+package com.example.demo.service;
+import com.example.demo.entity.StudentEntity;
+public interface Studentservice{
+    StudentEntity post
+}
