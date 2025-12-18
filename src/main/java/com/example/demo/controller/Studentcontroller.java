b9446105244f7c7c3e4b026e72bfe30f286ffbd1
bala-amypo <bala@amypo.in>
2025-12-18 06:21:42 +0000

    User commit

--- src/main/java/com/example/demo/controller/Studentcontroller.java
+++ src/main/java/com/example/demo/controller/Studentcontroller.java
@@ -30,7 +30,7 @@ public class StudentController{
         return service.getData(id);
     }
     @PutMapping("put/{id}")
-    public StudentEntity putid(@PathVariable int id,StudentEntity entity){
-        return service.putData(id);
+    public StudentEntity putvalId(@PathVariable int id,@RequestBodey StudentEntity entity){
+        return service.putData(id,entity);
     }
 }
