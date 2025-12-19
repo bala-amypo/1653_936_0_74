a974602642a7670aed34a58a20b91824ccbc111b
bala-amypo <bala@amypo.in>
2025-12-19 07:54:58 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/Timestampserviceimpl.java
+++ src/main/java/com/example/demo/service/impl/Timestampserviceimpl.java
@@ -1,15 +1,16 @@
 package com.example.demo.service.Impl;
 
-import com.example.demo.service.Studentservice;
+import com.example.demo.service.Timestampservice;
 import org.springframework.stereotype.Service;
-import com.example.demo.entity.StudentEntity;
-import com.example.demo.repository.Studentrepository;
+import com.example.demo.entity.TimestampEntity;
+import com.example.demo.repository.Timestamprepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import java.util.List;
 @Service
-public class Studentserviceimpl implements Studentservice{
-    @Autowired Studentrepository student;
+public class Timestampserviceimpl implements Timestampservice{
+    @Autowired Timestamprepository student;
     @Override
-    public StudentEntity postdata(StudentEntity stu){
+    public TimestampEntity postdata(TimestampEntity stu){
         return student.save(stu);
     }
+}
