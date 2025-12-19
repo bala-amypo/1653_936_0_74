705e4a6ed4351ccb6647e69c3823dc1c1f916b92
bala-amypo <bala@amypo.in>
2025-12-19 10:30:58 +0000

    User commit

--- src/main/java/com/example/demo/service/Detailservice.java
+++ src/main/java/com/example/demo/service/Detailservice.java
@@ -3,5 +3,5 @@ package com.example.demo.service;
 import java.util.List;
 import com.example.demo.entity.DetailsEntity;
 public interface Detailservice{
-    DetailsEntity postdata(DetailsEntity entity);
+    DetailsEntity postDetail(DetailsEntity entity);
 }
