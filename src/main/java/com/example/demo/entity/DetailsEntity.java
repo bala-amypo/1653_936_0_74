d70c95fdc912a50274b6caf08fe591fed1adf610
bala-amypo <bala@amypo.in>
2025-12-19 10:00:58 +0000

    User commit

--- src/main/java/com/example/demo/entity/DetailsEntity.java
+++ src/main/java/com/example/demo/entity/DetailsEntity.java
@@ -22,8 +22,8 @@ public class DetailsEntity{
     private Integer id;
     private int cardnum;
     @OneToOne
-    private StudentDatabase student;
     @JoinColumn(name = "student_id")
+    private StudentDatabaseEntity student;
 
 
 }
