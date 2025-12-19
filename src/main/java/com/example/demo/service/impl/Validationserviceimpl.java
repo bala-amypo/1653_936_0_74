b7f6c1be022cc7e39f546ffe1a861a178f71b1da
bala-amypo <bala@amypo.in>
2025-12-19 05:02:36 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/Validationserviceimpl.java
+++ src/main/java/com/example/demo/service/impl/Validationserviceimpl.java
@@ -6,6 +6,7 @@ import com.example.demo.entity.ValidationEntity;
 import com.example.demo.repository.ValidationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import java.util.List;
+import com.example.demo.exception.ValidationException;
 @Service
 public class Validationserviceimpl implements Validationservice{
     @Autowired ValidationRepository student;
